d3c404fc333da35e3e9fbba6027596711d0627af
Rene <rj.gescher@gmail.com>
2025-11-30 17:55:33 +0100

    v1

---
+++ config.js
@@ -0,0 +1,7 @@
+window.APP_CONFIG = {
+
+    APP_SUPABASE_URL = "https://sjhyvctidpnttqhenqoy.supabase.co";
+    APP_SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqaHl2Y3RpZHBudHRxaGVucW95Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ0MzU3NTksImV4cCI6MjA4MDAxMTc1OX0.oXUA012J99W8fNNJi8Zxqn-2BUjf1QJOrXGYlPotaS4"; 
+    MASTER_ACCESS_CODE = "123456"; // <<<<<<<<<<<<<<< WICHTIG: Setzen Sie hier Ihren Code fest
+
+};
